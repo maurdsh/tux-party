4bfea8c7a6e624cf8f691845894153c367136a71
maurdsh <maurigg0703@gmail.com>
2024-09-21 20:33:09 -0600

    Update server.js

--- server.js
+++ server.js
@@ -43,5 +43,6 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(Servidor escuchando en el puerto ${PORT});
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
+
