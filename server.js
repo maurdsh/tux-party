f3c46aafa981bc0dcedb16179adac24a48ae3c84
maurdsh <maurigg0703@gmail.com>
2024-10-17 20:46:53 -0600

    Update server.js

--- server.js
+++ server.js
@@ -36,6 +36,18 @@ io.on('connection', (socket) => {
         io.emit('receiveMessage', { id: socket.id, message: message }); // Emitir el mensaje para todos
     });
 
+    // Manejar comandos especiales como 'exit' y 'nescafe'
+    socket.on('exit', () => {
+        console.log(`El jugador ${socket.id} ha salido.`);
+        socket.disconnect(); // Desconectar al usuario si ejecuta el comando 'exit'
+    });
+
+    socket.on('nescafe', () => {
+        console.log(`El jugador ${socket.id} pidió un Nescafé.`);
+        io.emit('receiveMessage', { id: socket.id, message: 'Aquí tienes un Nescafé para todos!' }); 
+        // Emitir un mensaje a todos los usuarios
+    });
+
     // Manejar la desconexión de un jugador
     socket.on('disconnect', () => {
         console.log('Jugador desconectado:', socket.id);
@@ -48,3 +60,4 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
+
